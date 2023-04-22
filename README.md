b48940b68356003c54a3eaf52393e4eda2da3195
sixtusagbo <miracleagbosixtus@gmail.com>
2023-04-22 12:58:44 +0100

    Connect to MetaMask

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Swapper
 A token swap dapp with [0x](https://0x.org) API
+
+# Overview
+I use a global API at `window.ethereum` injected by MetaMask into websites visited by its users.
+This API allows me to interact with MetaMask in order to request users' Ethereum accounts, read data from blockchains the user is connected to, and suggest that the user sign messages and transactions.The presence of the provider object indicates an Ethereum user and that MetaMask is installed.
---
+++ index.js
@@ -0,0 +1,32 @@
+async function connect() {
+  // Check if MetaMask is installed, if it is, try connecting to an account
+  if (typeof window.ethereum !== 'undefined') {
+    try {
+      console.log('connecting');
+      // Requests that the user provides an Ethereum address to be identified by.
+      // This request causes a MetaMask popup to appear.
+      // More at: https://docs.metamask.io/wallet/reference/rpc-api/#eth-requestaccounts
+      await ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (error) {
+      if (error.code === 4001) {
+        // EIP-1193: userRejectedRequest error
+        alert('You have to approve the request in order to use this app.');
+        console.log('User rejected request!');
+      } else {
+        console.error(error);
+      }
+    }
+    // If connected, change button to "Connected"
+    document.getElementById('login_button').innerHTML = 'Connected';
+    // If connected, enable "Swap" button
+    document.getElementById('swap_button').disabled = false;
+  }
+  // Ask user to install MetaMask if it's not detected
+  else {
+    document.getElementById('login_button').innerHTML =
+      'Please install MetaMask';
+  }
+}
+
+// Call the connect function when the login_button is clicked
+document.getElementById('login_button').onclick = connect;
